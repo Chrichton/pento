0e10fe323b959aa8a484c8d236c2646f03b71e20
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-04-22 17:21:49 +0200

    remove username, password: "postgres"

--- config/test.exs
+++ config/test.exs
@@ -9,8 +9,6 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :pento, Pento.Repo,
-  username: "postgres",
-  password: "postgres",
   database: "pento_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
