99bd0527aa56e4e85c077760c7be750f5a2c64bf
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-04-25 08:12:43 +0200

    fixed disconnected and connected render

--- test/pento_web/live/page_live_test.exs
+++ test/pento_web/live/page_live_test.exs
@@ -4,8 +4,8 @@ defmodule PentoWeb.PageLiveTest do
   import Phoenix.LiveViewTest
 
   test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    {:error, {:redirect, %{to: path}}} = live(conn, "/")
+
+    assert path == "/users/log_in"
   end
 end
