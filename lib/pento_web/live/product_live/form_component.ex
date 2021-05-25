a15b1926b8496e9a15b44d646af24a64034937c1
Heiko Goes <heiko.goes@gaebler-goes.de>
2021-05-25 07:03:51 +0200

    removed sleep

--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -20,8 +20,6 @@ defmodule PentoWeb.ProductLive.FormComponent do
   end
 
   defp handle_progress(:image, entry, socket) do
-    :timer.sleep(1000)
-
     if entry.done? do
       path =
         consume_uploaded_entry(
